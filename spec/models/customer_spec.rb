ce0904f6fa8fd0a63454bc4be0ef4e3dd10ddf30
moshe.ribeiro <moises.ribeiro.developer@gmail.com>
2023-10-03 18:00:07 -0300

    Aprendendo o attributes_for

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to eq('Sr. Moshé')
   end
 
+  it 'Usando o attributes_for' do
+    attrs = attributes_for(:customer)
+    customer = Customer.create(attrs)
+    expect(customer.full_name).to start_with('Sr')
+  end
+
   it { expect{create(:customer)}.to change{Customer.all.size}.by(1) }
 end
