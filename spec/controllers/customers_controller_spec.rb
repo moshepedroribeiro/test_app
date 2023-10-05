12082f2c8e41c32f9a51fd75065f72eb1864001b
moshe.ribeiro <moises.ribeiro.developer@gmail.com>
2023-10-05 09:24:48 -0300

    Finalizando o módulo 4 do curso.

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -26,23 +26,54 @@ RSpec.describe CustomersController, type: :controller do
   end
 
   describe 'As a Logged Member' do
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    it 'Routes' do
+      is_expected.to route(:get, '/customers').to(action: :index)
+    end
+
+    it 'Content-Type JSON' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, format: :json, params: { customer: customer_params }
+      expect(response.content_type).to match(/application\/json/)
+    end
+
+    it 'Flash Notice' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, params: { customer: customer_params }
+      expect(flash[:notice]).to match(/successfully created/)
+    end
+
+    it 'with a valid attributes' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      expect {
+        post :create, params: { customer: customer_params }
+      }.to change(Customer, :count).by(1)
+    end
+
+    it 'with a invalid attributes' do
+      customer_params = attributes_for(:customer, address: nil)
+      sign_in @member
+      expect {
+        post :create, params: { customer: customer_params }
+      }.not_to change(Customer, :count)
+    end
+
     it '#show' do
-      member = create(:member)
-      customer = create(:customer)
-
-      sign_in member
-
-      get :show, params: { id: customer.id }
+      sign_in @member
+      get :show, params: { id: @customer.id }
       expect(response).to have_http_status(200)
     end
 
     it 'render a :show template' do
-      member = create(:member)
-      customer = create(:customer)
-
-      sign_in member
-
-      get :show, params: { id: customer.id }
+      sign_in @member
+      get :show, params: { id: @customer.id }
       expect(response).to render_template(:show)
     end
   end
